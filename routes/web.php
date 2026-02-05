eb68814e6f5b4e5b7fe6fe0a3e812d993c43dcba
mukhamadkalwani11025 <mukhamadkalwani1@gmail.com>
2026-02-05 14:35:11 +0700

    first web.php

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,19 @@ use App\Http\Controllers\KriteriaController;
 use App\Http\Controllers\MakananKriteriaController;
 use App\Http\Controllers\RekomendasiController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| AUTH
-|--------------------------------------------------------------------------
-*/
-Route::get('/', [LoginController::class, 'showLoginForm']);
-Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+// auth
+Auth::routes();
+Route::get('/', function () {return redirect('/login');});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-/*
-|--------------------------------------------------------------------------
-| PROTECTED ROUTES
-|--------------------------------------------------------------------------
-*/
+// controller tabel
+Route::resource('makanan', MakananController::class);
+Route::resource('kriteria', KriteriaController::class);
+Route::resource('makanan_kriteria',MakananKriteriaController::class);
+Route::resource('rekomendasi', RekomendasiController::class);
+
 Route::middleware(['auth'])->group(function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::resource('makanan', MakananController::class);
-    Route::resource('kriteria', KriteriaController::class);
-    Route::resource('makanan_kriteria', MakananKriteriaController::class);
-
-    // Rekomendasi (SAW)
     Route::get('/rekomendasi', [RekomendasiController::class, 'index'])->name('rekomendasi.index');
     Route::post('/rekomendasi/hitung', [RekomendasiController::class, 'hitung'])->name('rekomendasi.hitung');
     Route::delete('/rekomendasi/{id}', [RekomendasiController::class, 'destroy'])->name('rekomendasi.destroy');
